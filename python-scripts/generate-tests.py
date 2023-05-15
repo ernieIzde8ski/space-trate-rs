e2ea7fd9047ff1f44c872cec9ce7021433b41e3c
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2023-05-14 23:33:36 -0400

    move generate-tests.py, add src/tests/

--- python-scripts/generate-tests.py
+++ python-scripts/generate-tests.py
@@ -7,7 +7,22 @@ Spits out tests to dump into lib.rs for each file in the schema-examples directo
 import re
 from pathlib import Path
 
-schema_dir = Path(__file__).with_name("schema-examples")
+root = Path(__file__).parent.parent
+schema_dir = root / "schema-examples"
+target_file = root / "src" / "tests" / "schema.rs"
+
+
+start = r"""use crate::api::schemas;
+
+macro_rules! json_deserialize_test {
+    ($name:ident, $struct_type:ty, $file_path:expr) => {
+        #[test]
+        fn $name() {
+            let string = std::fs::read_to_string($file_path).unwrap();
+            serde_json::from_str::<$struct_type>(&string).unwrap();
+        }
+    };
+}"""
 
 
 def title_to_snake(s: str, /) -> str:
@@ -23,8 +38,12 @@ def main():
     # iterate through files and match by their names
     # it should output a bunch of lines looking like the following:
     #   json_deserialize_test!(test_contract_schema,schemas::Contract,"schema-examples/Contract.json");
-    for file in schema_dir.glob("*.json"):
-        title_name = file.name.removesuffix(".json")
+
+    file = open(target_file, "w+", encoding="utf-8")
+    file.write(start)
+    file.write("\n\n")
+    for path in schema_dir.glob("*.json"):
+        title_name = path.name.removesuffix(".json")
         snake_name = title_to_snake(title_name)
         print(
             "json_deserialize_test!(test_",
@@ -32,9 +51,12 @@ def main():
             "_schema, schemas::",
             title_name,
             ', "schema-examples/',
-            file.name,
+            path.name,
             '");',
             sep="",
+            file=file,
         )
 
-if __name__ == "__main__": main()
+
+if __name__ == "__main__":
+    main()
--- src/lib.rs
+++ src/lib.rs
@@ -41,41 +41,18 @@ impl SpaceTraderAgent {
             .form(&params)
             .send()
             .await?
-            .json::<ApiResult<RegistryResponse>>()
+            .json::<ApiResult<i32>>()
             .await?
             .to_result()?;
 
         Ok(SpaceTraderAgent {
             client: client,
-            token: result.token,
+            token: "result.token".to_string(),
         })
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::schemas;
-
-    macro_rules! json_deserialize_test {
-        ($name:ident, $struct_type:ty, $file_path:expr) => {
-            #[test]
-            fn $name() {
-                let string = std::fs::read_to_string($file_path).unwrap();
-                serde_json::from_str::<$struct_type>(&string).unwrap();
-            }
-        };
-    }
-
-    json_deserialize_test!(test_market_schema, schemas::Market, "schema-examples/Market.json");
-    json_deserialize_test!(test_ship_schema, schemas::Ship, "schema-examples/Ship.json");
-    json_deserialize_test!(test_shipyard_schema, schemas::Shipyard, "schema-examples/Shipyard.json");
-    json_deserialize_test!(test_waypoint_schema, schemas::Waypoint, "schema-examples/Waypoint.json");
-    json_deserialize_test!(test_ship_nav_schema, schemas::ShipNav, "schema-examples/ShipNav.json");
-    json_deserialize_test!(test_system_schema, schemas::System, "schema-examples/System.json");
-    json_deserialize_test!(test_waypoint_trait_schema, schemas::WaypointTrait, "schema-examples/WaypointTrait.json");
-    json_deserialize_test!(test_contract_schema, schemas::Contract, "schema-examples/Contract.json");
-    json_deserialize_test!(test_survey_schema, schemas::Survey, "schema-examples/Survey.json");
-    json_deserialize_test!(test_faction_schema, schemas::Faction, "schema-examples/Faction.json");
-    json_deserialize_test!(test_scanned_waypoint_schema, schemas::ScannedWaypoint, "schema-examples/ScannedWaypoint.json");
-    json_deserialize_test!(test_jump_gate_schema, schemas::JumpGate, "schema-examples/JumpGate.json");
+    mod schema;
 }
